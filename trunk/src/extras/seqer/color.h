0403c463cd08a5cc67595808495d9ff6110a4c58
divbyzero@gmail.com <divbyzero@gmail.com@08fcf85c-1fc8-6ccc-a709-d29ba5ab2852>
2014-07-29 01:04:33 +0000

    initial piano roll drawing, with colors automatically adapted from the system settings

---
+++ trunk/src/extras/seqer/color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_INCLUDED
+#define COLOR_INCLUDED
+
+// RGB = [0, 256); H = [0, 360); S = [0, 100); L = [0, 100)
+void RgbToHsl(const unsigned int r, const unsigned int g, const unsigned int b, unsigned int& h, unsigned int& s, unsigned int& l);
+void HslToRgb(const unsigned int h, const unsigned int s, const unsigned int l, unsigned int& r, unsigned int& g, unsigned int& b);
+
+wxColour ColorShade(const wxColour input, const unsigned int luminance);
+
+#endif
