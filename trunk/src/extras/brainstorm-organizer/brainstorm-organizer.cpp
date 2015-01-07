90887c6a06ff3f106bb2dde92088c7a718dc4478
divbyzero <divbyzero@08fcf85c-1fc8-6ccc-a709-d29ba5ab2852>
2015-01-07 23:14:20 +0000

    initial directory/file handling

--- trunk/src/extras/brainstorm-organizer/brainstorm-organizer.cpp
+++ trunk/src/extras/brainstorm-organizer/brainstorm-organizer.cpp
@@ -1,17 +1,21 @@
 
+#include <wx/dir.h>
+#include <wx/regex.h>
 #include <wx/wx.h>
 #include <midifile.h>
 
 class Application: public wxApp
 {
 private:
-	wxTextCtrl* dirTextBox;
+	wxFrame* window;
 	wxListBox* filesListBox;
 	wxStaticText* timeLabel;
 	wxTextCtrl* nameTextBox;
+	std::string dir;
 
 public:
 	bool OnInit();
+	void OnOpen(wxCommandEvent& event);
 	void OnRefresh(wxCommandEvent& event);
 	void OnUp(wxCommandEvent& event);
 	void OnDown(wxCommandEvent& event);
@@ -20,96 +24,134 @@ public:
 	void OnBack(wxCommandEvent& event);
 	void OnForward(wxCommandEvent& event);
 	void OnRename(wxCommandEvent& event);
+	void ListFiles();
 };
 
 IMPLEMENT_APP(Application)
 
 bool Application::OnInit()
 {
-	wxFrame* window = new wxFrame((wxFrame*)(NULL), wxID_ANY, "Brainstorm Organizer", wxDefaultPosition, wxSize(640, 480));
-	this->SetTopWindow(window);
+	this->window = new wxFrame((wxFrame*)(NULL), wxID_ANY, "Brainstorm Organizer", wxDefaultPosition, wxSize(640, 480));
+	this->SetTopWindow(this->window);
 
 	wxFlexGridSizer* outerSizer = new wxFlexGridSizer(2);
 	outerSizer->AddGrowableCol(0);
-	outerSizer->AddGrowableRow(1);
-	window->SetSizer(outerSizer);
+	outerSizer->AddGrowableRow(0);
+	this->window->SetSizer(outerSizer);
 
-	this->dirTextBox = new wxTextCtrl(window, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
-	this->dirTextBox->Bind(wxEVT_TEXT_ENTER, &Application::OnRefresh, this);
-	outerSizer->Add(this->dirTextBox, 0, wxEXPAND | wxALL, 4);
-
-	wxButton* refreshButton = new wxButton(window, wxID_ANY, "&Refresh");
-	refreshButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnRefresh, this);
-	outerSizer->Add(refreshButton, 0, wxEXPAND | wxTOP | wxRIGHT | wxBOTTOM, 4);
-
-	this->filesListBox = new wxListBox(window, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE | wxLB_ALWAYS_SB);
-	outerSizer->Add(this->filesListBox, 1, wxEXPAND | wxRIGHT | wxBOTTOM | wxLEFT, 4);
+	this->filesListBox = new wxListBox(this->window, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE | wxLB_ALWAYS_SB);
+	outerSizer->Add(this->filesListBox, 1, wxEXPAND | wxALL, 4);
 
 	wxBoxSizer* rightSizer = new wxBoxSizer(wxVERTICAL);
 	outerSizer->Add(rightSizer, 0, wxEXPAND);
 
-	rightSizer->AddStretchSpacer();
+	wxButton* openButton = new wxButton(this->window, wxID_ANY, "&Open");
+	openButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnOpen, this);
+	rightSizer->Add(openButton, 0, wxEXPAND | wxTOP | wxRIGHT | wxBOTTOM, 4);
 
-	wxButton* upButton = new wxButton(window, wxID_ANY, "&Up");
+	wxButton* refreshButton = new wxButton(this->window, wxID_ANY, "&Refresh");
+	refreshButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnRefresh, this);
+	rightSizer->Add(refreshButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
+
+	rightSizer->AddSpacer(32);
+
+	wxButton* upButton = new wxButton(this->window, wxID_ANY, "&Up");
 	upButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnUp, this);
 	rightSizer->Add(upButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	wxButton* downButton = new wxButton(window, wxID_ANY, "&Down");
+	wxButton* downButton = new wxButton(this->window, wxID_ANY, "&Down");
 	downButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnDown, this);
 	rightSizer->Add(downButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	rightSizer->AddStretchSpacer();
+	rightSizer->AddSpacer(32);
 
-	wxButton* playButton = new wxButton(window, wxID_ANY, "&Play");
+	wxButton* playButton = new wxButton(this->window, wxID_ANY, "&Play");
 	playButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnPlay, this);
 	rightSizer->Add(playButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	wxButton* stopButton = new wxButton(window, wxID_ANY, "&Stop");
+	wxButton* stopButton = new wxButton(this->window, wxID_ANY, "&Stop");
 	stopButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnStop, this);
 	rightSizer->Add(stopButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	wxButton* backButton = new wxButton(window, wxID_ANY, "&Back");
+	wxButton* backButton = new wxButton(this->window, wxID_ANY, "&Back");
 	backButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnBack, this);
 	rightSizer->Add(backButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	wxButton* forwardButton = new wxButton(window, wxID_ANY, "&Forward");
+	wxButton* forwardButton = new wxButton(this->window, wxID_ANY, "&Forward");
 	forwardButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnForward, this);
 	rightSizer->Add(forwardButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
 	rightSizer->AddSpacer(4);
-	this->timeLabel = new wxStaticText(window, wxID_ANY, "0 of 0 s");
+	this->timeLabel = new wxStaticText(this->window, wxID_ANY, "0 of 0 s");
 	rightSizer->Add(this->timeLabel, 0, wxALIGN_CENTER | wxRIGHT | wxBOTTOM, 4);
 
-	rightSizer->AddStretchSpacer();
-
-	this->nameTextBox = new wxTextCtrl(window, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
+	this->nameTextBox = new wxTextCtrl(this->window, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER);
 	this->nameTextBox->Bind(wxEVT_TEXT_ENTER, &Application::OnRename, this);
 	outerSizer->Add(this->nameTextBox, 1, wxEXPAND | wxRIGHT | wxBOTTOM | wxLEFT, 4);
 
-	wxButton* renameButton = new wxButton(window, wxID_ANY, "Re&name");
+	wxButton* renameButton = new wxButton(this->window, wxID_ANY, "Re&name");
 	renameButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Application::OnRename, this);
 	outerSizer->Add(renameButton, 0, wxEXPAND | wxRIGHT | wxBOTTOM, 4);
 
-	window->Show(true);
+	this->dir = ".";
+	this->ListFiles();
+
+	this->window->Show(true);
 	return true;
+}
+
+void Application::OnOpen(wxCommandEvent& WXUNUSED(event))
+{
+	wxDirDialog dialog(this->window, wxDirSelectorPromptStr, "", wxDD_DEFAULT_STYLE | wxDD_DIR_MUST_EXIST);
+
+	if (dialog.ShowModal() == wxID_OK)
+	{
+		this->dir = dialog.GetPath();
+		this->ListFiles();
+	}
 }
 
 void Application::OnRefresh(wxCommandEvent& WXUNUSED(event))
 {
-	this->timeLabel->SetLabelText("refresh");
-	this->timeLabel->SendSizeEventToParent();
+	this->ListFiles();
 }
 
 void Application::OnUp(wxCommandEvent& WXUNUSED(event))
 {
-	this->timeLabel->SetLabelText("up");
-	this->timeLabel->SendSizeEventToParent();
+	int selection = this->filesListBox->GetSelection();
+
+	if (selection == wxNOT_FOUND)
+	{
+		selection = 0;
+	}
+	else
+	{
+		selection--;
+	}
+
+	if ((selection >= 0) && (this->filesListBox->GetCount() > 0))
+	{
+		this->filesListBox->SetSelection(selection);
+	}
 }
 
 void Application::OnDown(wxCommandEvent& WXUNUSED(event))
 {
-	this->timeLabel->SetLabelText("down");
-	this->timeLabel->SendSizeEventToParent();
+	int selection = this->filesListBox->GetSelection();
+
+	if (selection == wxNOT_FOUND)
+	{
+		selection = 0;
+	}
+	else
+	{
+		selection++;
+	}
+
+	if (selection < this->filesListBox->GetCount())
+	{
+		this->filesListBox->SetSelection(selection);
+	}
 }
 
 void Application::OnPlay(wxCommandEvent& WXUNUSED(event))
@@ -140,5 +182,31 @@ void Application::OnRename(wxCommandEvent& WXUNUSED(event))
 {
 	this->timeLabel->SetLabelText("rename");
 	this->timeLabel->SendSizeEventToParent();
+}
+
+void Application::ListFiles()
+{
+   wxDir dir(this->dir);
+
+	if (dir.IsOpened())
+	{
+		this->filesListBox->Clear();
+
+		wxRegEx filenamePattern("\\d{14}\\.mid$", wxRE_ADVANCED);
+		wxString filename;
+
+		for (bool hasMoreFiles = dir.GetFirst(&filename, "", wxDIR_FILES); hasMoreFiles; hasMoreFiles = dir.GetNext(&filename))
+		{
+			if (filenamePattern.Matches(filename))
+			{
+				this->filesListBox->Append(filename);
+			}
+		}
+
+		if (this->filesListBox->GetCount() > 0)
+		{
+			this->filesListBox->SetSelection(0);
+		}
+	}
 }
 
