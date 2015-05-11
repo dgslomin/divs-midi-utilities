c6d25decfcce6d5e936ccde74098b6f774dc3e9c
divbyzero@gmail.com <divbyzero@gmail.com@08fcf85c-1fc8-6ccc-a709-d29ba5ab2852>
2015-05-11 01:35:12 +0000

    playback working (mostly) on windows

--- trunk/src/extras/brainstorm-organizer/brainstorm-organizer-support-macos.cpp
+++ trunk/src/extras/brainstorm-organizer/brainstorm-organizer-support-macos.cpp
@@ -8,73 +8,73 @@
 class MidiOutput::Impl
 {
 public:
-    MIDIClientRef midiClient;
-    MIDIPortRef midiOutputPort;
-    MIDIEndpointRef midiDestination;
+	MIDIClientRef midiClient;
+	MIDIPortRef midiOutputPort;
+	MIDIEndpointRef midiDestination;
 };
 
 MidiOutput::MidiOutput()
 {
-    this->impl = new Impl();
-    MIDIClientCreate(CFSTR("Brainstorm Organizer"), NULL, NULL, &(this->impl->midiClient));
-    MIDIOutputPortCreate(this->impl->midiClient, CFSTR("playback"), &(this->impl->midiOutputPort));
-    this->impl->midiDestination = kMIDIUnknownEndpoint;
+	this->impl = new Impl();
+	MIDIClientCreate(CFSTR("Brainstorm Organizer"), NULL, NULL, &(this->impl->midiClient));
+	MIDIOutputPortCreate(this->impl->midiClient, CFSTR("playback"), &(this->impl->midiOutputPort));
+	this->impl->midiDestination = kMIDIUnknownEndpoint;
 }
 
 MidiOutput::~MidiOutput()
 {
-    MIDIPortDispose(this->impl->midiOutputPort);
-    MIDIClientDispose(this->impl->midiClient);
-    delete this->impl;
+	MIDIPortDispose(this->impl->midiOutputPort);
+	MIDIClientDispose(this->impl->midiClient);
+	delete this->impl;
 }
 
 void MidiOutput::DisplayConfigDialog()
 {
-    wxDialog* dialog = new wxDialog(NULL, wxID_ANY, "Brainstorm Organizer");
+	wxDialog* dialog = new wxDialog(NULL, wxID_ANY, "Brainstorm Organizer");
 
-    wxBoxSizer* outerSizer = new wxBoxSizer(wxVERTICAL);
-    dialog->SetSizer(outerSizer);
+	wxBoxSizer* outerSizer = new wxBoxSizer(wxVERTICAL);
+	dialog->SetSizer(outerSizer);
 
-    wxListBox* destinationList = new wxListBox(dialog, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE);
-    outerSizer->Add(destinationList, 1, wxEXPAND | wxTOP | wxRIGHT | wxBOTTOM | wxLEFT, 4);
+	wxListBox* destinationList = new wxListBox(dialog, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE);
+	outerSizer->Add(destinationList, 1, wxEXPAND | wxTOP | wxRIGHT | wxBOTTOM | wxLEFT, 4);
 
-    wxSizer* buttonSizer = dialog->CreateButtonSizer(wxOK | wxCANCEL);
-    outerSizer->Add(buttonSizer, 0, wxRIGHT | wxBOTTOM | wxLEFT, 4);
-    
-    for (int destinationNumber = 0; destinationNumber < MIDIGetNumberOfDestinations(); destinationNumber++)
-    {
-        MIDIEndpointRef midiDestination = MIDIGetDestination(destinationNumber);
-        CFStringRef midiDestinationName;
-        MIDIObjectGetStringProperty(midiDestination, kMIDIPropertyDisplayName, &midiDestinationName);
-        destinationList->Append(wxCFStringRef(midiDestinationName).AsString());
-        if (midiDestination == this->impl->midiDestination) destinationList->SetSelection(destinationNumber);
-    }
+	wxSizer* buttonSizer = dialog->CreateButtonSizer(wxOK | wxCANCEL);
+	outerSizer->Add(buttonSizer, 0, wxRIGHT | wxBOTTOM | wxLEFT, 4);
+	
+	for (int destinationNumber = 0; destinationNumber < MIDIGetNumberOfDestinations(); destinationNumber++)
+	{
+		MIDIEndpointRef midiDestination = MIDIGetDestination(destinationNumber);
+		CFStringRef midiDestinationName;
+		MIDIObjectGetStringProperty(midiDestination, kMIDIPropertyDisplayName, &midiDestinationName);
+		destinationList->Append(wxCFStringRef(midiDestinationName).AsString());
+		if (midiDestination == this->impl->midiDestination) destinationList->SetSelection(destinationNumber);
+	}
 
-    if (dialog->ShowModal() == wxID_OK)
-    {
-        int destinationNumber = destinationList->GetSelection();
-        this->impl->midiDestination = (destinationNumber == wxNOT_FOUND) ? kMIDIUnknownEndpoint : MIDIGetDestination(destinationNumber);
-    }
+	if (dialog->ShowModal() == wxID_OK)
+	{
+		int destinationNumber = destinationList->GetSelection();
+		this->impl->midiDestination = (destinationNumber == wxNOT_FOUND) ? kMIDIUnknownEndpoint : MIDIGetDestination(destinationNumber);
+	}
 
-    dialog->Destroy();
+	dialog->Destroy();
 }
 
 void MidiOutput::SendMessage(MidiFileEvent_t event)
 {
-    if ((this->impl->midiDestination == kMIDIUnknownEndpoint) || !MidiFileEvent_isVoiceEvent(event)) return;
+	if ((this->impl->midiDestination == kMIDIUnknownEndpoint) || !MidiFileEvent_isVoiceEvent(event)) return;
 
-    union
-    {
-        unsigned long messageAsUint32;
-        Byte messageAsBytes[4];
-    }
-    u;
+	union
+	{
+		unsigned long messageAsUint32;
+		Byte messageAsBytes[4];
+	}
+	u;
 
-    u.messageAsUint32 = MidiFileVoiceEvent_getData(event);
-    Byte midiPacketBuffer[128];
-    MIDIPacketList* midiPacketList = (MIDIPacketList*)(midiPacketBuffer);
-    MIDIPacket* midiPacket = MIDIPacketListInit(midiPacketList);
-    midiPacket = MIDIPacketListAdd(midiPacketList, sizeof (midiPacketBuffer), midiPacket, 0, MidiFileVoiceEvent_getDataLength(event), u.messageAsBytes);
-    MIDISend(this->impl->midiOutputPort, this->impl->midiDestination, midiPacketList);
+	u.messageAsUint32 = MidiFileVoiceEvent_getData(event);
+	Byte midiPacketBuffer[128];
+	MIDIPacketList* midiPacketList = (MIDIPacketList*)(midiPacketBuffer);
+	MIDIPacket* midiPacket = MIDIPacketListInit(midiPacketList);
+	midiPacket = MIDIPacketListAdd(midiPacketList, sizeof (midiPacketBuffer), midiPacket, 0, MidiFileVoiceEvent_getDataLength(event), u.messageAsBytes);
+	MIDISend(this->impl->midiOutputPort, this->impl->midiDestination, midiPacketList);
 }
 
--- trunk/src/extras/brainstorm-organizer/brainstorm-organizer-support-win32.cpp
+++ trunk/src/extras/brainstorm-organizer/brainstorm-organizer-support-win32.cpp
@@ -1,45 +1,64 @@
 
+#include <wx/wx.h>
 #include <Windows.h>
 #include <brainstorm-organizer-support.h>
 
 class MidiOutput::Impl
 {
 public:
-    HMIDIOUT midiOut;
+	int midiOutNumber;
+	HMIDIOUT midiOut;
 };
 
 MidiOutput::MidiOutput()
 {
-    int midiOutNumber = MIDI_MAPPER;
-    this->impl = new Impl();
-	midiOutOpen(&(this->impl->midiOut), midiOutNumber, (DWORD_PTR)(NULL), (DWORD_PTR)(NULL), CALLBACK_NULL);
+	this->impl = new Impl();
+	this->impl->midiOutNumber = MIDI_MAPPER;
+	midiOutOpen(&(this->impl->midiOut), this->impl->midiOutNumber, (DWORD_PTR)(NULL), (DWORD_PTR)(NULL), CALLBACK_NULL);
 }
 
 MidiOutput::~MidiOutput()
 {
-    midiOutClose(this->impl->midiOut);
-    delete this->impl;
+	midiOutClose(this->impl->midiOut);
+	delete this->impl;
 }
 
 void MidiOutput::DisplayConfigDialog()
 {
-    // TODO: display modally; allow user to select
-    // midiOutClose(this->impl->midiOut);
-	// midiOutOpen(&(this->impl->midiOut), midiOutNumber, (DWORD_PTR)(NULL), (DWORD_PTR)(NULL), CALLBACK_NULL);
+	wxDialog* dialog = new wxDialog(NULL, wxID_ANY, "Brainstorm Organizer");
 
-    for (int midiOutNumber = 0; midiOutNumber < midiOutGetNumDevs(); midiOutNumber++)
-    {
-        MIDIOUTCAPS midiOutCaps;
-        midiOutGetDevCaps(midiOutNumber, &midiOutCaps, sizeof(midiOutCaps));
-        char* midiOutName = midiOutCaps.szPname;
-    }
+	wxBoxSizer* outerSizer = new wxBoxSizer(wxVERTICAL);
+	dialog->SetSizer(outerSizer);
+
+	wxListBox* midiOutList = new wxListBox(dialog, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, wxLB_SINGLE);
+	outerSizer->Add(midiOutList, 1, wxEXPAND | wxTOP | wxRIGHT | wxBOTTOM | wxLEFT, 4);
+
+	wxSizer* buttonSizer = dialog->CreateButtonSizer(wxOK | wxCANCEL);
+	outerSizer->Add(buttonSizer, 0, wxRIGHT | wxBOTTOM | wxLEFT, 4);
+	
+	for (int midiOutNumber = 0; midiOutNumber < midiOutGetNumDevs(); midiOutNumber++)
+	{
+		MIDIOUTCAPS midiOutCaps;
+		midiOutGetDevCaps(midiOutNumber, &midiOutCaps, sizeof(midiOutCaps));
+		midiOutList->Append(wxString(midiOutCaps.szPname));
+		if (midiOutNumber == this->impl->midiOutNumber) midiOutList->SetSelection(midiOutNumber);
+	}
+
+	if (dialog->ShowModal() == wxID_OK)
+	{
+		midiOutClose(this->impl->midiOut);
+		this->impl->midiOutNumber = midiOutList->GetSelection();
+		midiOutOpen(&(this->impl->midiOut), this->impl->midiOutNumber, (DWORD_PTR)(NULL), (DWORD_PTR)(NULL), CALLBACK_NULL);
+	}
+
+	dialog->Destroy();
 }
 
 void MidiOutput::SendMessage(MidiFileEvent_t event)
 {
-    if (MidiFileEvent_isVoiceEvent(event))
-    {
-	    midiOutShortMsg(this->impl->midiOut, (DWORD)(MidiFileVoiceEvent_getData(event)));
-    }
+	if (MidiFileEvent_isVoiceEvent(event))
+	{
+		midiOutShortMsg(this->impl->midiOut, (DWORD)(MidiFileVoiceEvent_getData(event)));
+	}
 }
 
