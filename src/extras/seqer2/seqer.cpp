01bc386dbbcdd0292beb5e54629fc00b0ac5fbd8
Div Slomin <daslomin@microsoft.com>
2019-08-12 18:05:22 -0400

    still getting started

--- src/extras/seqer2/seqer.cpp
+++ src/extras/seqer2/seqer.cpp
@@ -1,10 +1,5 @@
 
-#include <functional>
-#include <set>
 #include <wx/wx.h>
-#include <wx/aboutdlg.h>
-#include <wx/filename.h>
-#include <wx/stdpaths.h>
 #include <midifile.h>
 #include "seqer.h"
 
@@ -27,32 +22,19 @@ bool Application::OnInit()
 	Window* window = new Window(this);
 	window->Show(true);
 
-	if (this->argc > 1)
-	{
-		if (!window->Load(this->argv[1]))
-		{
-			wxMessageBox("Cannot open the specified MIDI file.", "Error", wxOK | wxICON_ERROR);
-		}
-	}
-
 	return true;
 }
 
-TrackListWindow::TrackListWindow(Application* application, Window* existing_window): wxFrame((wxFrame*)(NULL), wxID_ANY, "Seqer", wxDefaultPosition, wxSize(640, 480))
+Window::Window(Application* application, Window* existing_window): wxFrame((wxFrame*)(NULL), wxID_ANY, "Seqer", wxDefaultPosition, wxSize(640, 480))
 {
 	this->application = application;
 	this->SetIcon(wxICON(seqer));
+	this->CreateStatusBar();
 
 	wxMenuBar* menu_bar = new wxMenuBar();
 		wxMenu* file_menu = new wxMenu();
 			file_menu->Append(wxID_NEW, "&New\tCtrl+N");
-			file_menu->AppendSeparator();
-			wxMenu* new_window_menu = new wxMenu();
-				new_window_menu->Append(SEQER_NEW_TRACK_LIST_WINDOW, "&Track List");
-				new_window_menu->Append(SEQER_NEW_PIANO_ROLL_WINDOW, "&Piano Roll");
-				new_window_menu->Append(SEQER_NEW_EVENT_LIST_WINDOW, "&Event List");
-			file_menu->AppendSubMenu(new_window_menu, "New &Window");
-			file_menu->AppendSeparator();
+			file_menu->Append(SEQER_ID_NEW_WINDOW, "New &Window\tCtrl+Shift+N");
 			file_menu->Append(wxID_OPEN, "&Open...\tCtrl+O");
 			file_menu->Append(wxID_SAVE, "&Save\tCtrl+S");
 			file_menu->Append(wxID_SAVEAS, "Save &As...\tCtrl+Shift+S");
@@ -71,6 +53,22 @@ TrackListWindow::TrackListWindow(Application* application, Window* existing_wind
 			edit_menu->Append(wxID_SELECTALL, "Select &All\tCtrl+A");
 			edit_menu->Append(SEQER_ID_SELECT_NONE, "Select &None\tCtrl+Shift+A");
 		menu_bar->Append(edit_menu, "&Edit");
+		wxMenu* view_menu = new wxMenu();
+			view_menu->Append(SEQER_ID_VIEW_TRACK_LIST, "&Track List", "", wxITEM_RADIO);
+			view_menu->Append(SEQER_ID_VIEW_PIANO_ROLL, "&Piano Roll", "", wxITEM_RADIO);
+			view_menu->Append(SEQER_ID_VIEW_EVENT_LIST, "&Event List", "", wxITEM_RADIO);
+		menu_bar->Append(view_menu, "&View");
+		wxMenu* track_menu = new wxMenu();
+			track_menu->Append(SEQER_ID_NEW_TRACK, "&New Track");
+			track_menu->AppendSeparator();
+			track_menu->Append(SEQER_ID_TRACK_RENAME, "&Rename");
+			track_menu->Append(SEQER_ID_TRACK_INPUT_PORT, "&Input Port");
+			track_menu->Append(SEQER_ID_TRACK_OUTPUT_PORT, "&Output Port");
+			track_menu->AppendSeparator();
+			track_menu->Append(SEQER_ID_ARM_TRACK, "&Arm", "", wxITEM_CHECK);
+			track_menu->Append(SEQER_ID_MUTE_TRACK, "&Mute", "", wxITEM_CHECK);
+			track_menu->Append(SEQER_ID_SOLO_TRACK, "&Solo", "", wxITEM_CHECK);
+		menu_bar->Append(track_menu, "&Track");
 		wxMenu* transport_menu = new wxMenu();
 			transport_menu->Append(SEQER_ID_PLAY, "&Play\tSpace");
 			transport_menu->Append(SEQER_ID_STOP, "&Stop\tShift+Space");
@@ -84,8 +82,6 @@ TrackListWindow::TrackListWindow(Application* application, Window* existing_wind
 			transport_menu->Append(SEQER_ID_PREVIOUS_MARKER, "Pre&vious Marker\tCtrl+Left");
 #endif
 			transport_menu->Append(SEQER_ID_GO_TO_MARKER, "Go to &Marker...\tCtrl+Shift+M");
-			transport_menu->AppendSeparator();
-			transport_menu->Append(SEQER_ID_PORTS, "P&orts...\tCtrl+Shift+O");
 		menu_bar->Append(transport_menu, "Trans&port");
 		wxMenu* tools_menu = new wxMenu();
 			tools_menu->Append(SEQER_ID_EXTERNAL_UTILITY, "External &Utility...\tCtrl+Shift+U");
@@ -105,286 +101,10 @@ TrackListWindow::TrackListWindow(Application* application, Window* existing_wind
 		menu_bar->Append(help_menu, "&Help");
 	this->SetMenuBar(menu_bar);
 
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		if (!this->SaveChangesIfNeeded()) return;
-		this->sequence_editor->New();
-	}, wxID_NEW);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		(new TrackListWindow(this->application, this))->Show(true);
-	}, SEQER_ID_NEW_TRACK_LIST_WINDOW);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		(new PianoRollWindow(this->application, this))->Show(true);
-	}, SEQER_ID_NEW_PIANO_ROLL_WINDOW);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		(new EventListWindow(this->application, this))->Show(true);
-	}, SEQER_ID_NEW_EVENT_LIST_WINDOW);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		if (!this->SaveChangesIfNeeded()) return;
-		wxFileDialog* file_dialog = new wxFileDialog(this, "Open File", "", "", "MIDI Files (*.mid)|*.mid|All files (*.*)|*.*", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-
-		if (file_dialog->ShowModal() == wxID_OK)
-		{
-			if (!this->Load(file_dialog->GetPath()))
-			{
-				wxMessageBox("Cannot open the specified MIDI file.", "Error", wxOK | wxICON_ERROR);
-			}
-		}
-
-		delete file_dialog;
-	}, wxID_OPEN);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		this->Save();
-	}, wxID_SAVE);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		this->SaveAs();
-	}, wxID_SAVEAS);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		this->Close();
-	}, wxID_CLOSE);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& event) {
-		std::set<Sequence*> sequences;
-		bool is_modified = false;
-
-		for (wxWindowList::compatibility_iterator window_iterator = wxTopLevelWindows.GetFirst(); window_iterator != NULL; window_iterator = window_iterator->GetNext())
-		{
-			Window* window = (Window*)(window_iterator->GetData());
-			sequences.insert(window->sequence);
-			if (window->sequence->IsModified()) is_modified = true;
-		}
-
-		if (sequences.size() == 1)
-		{
-			if (!is_modified || this->SaveChanges())
-			{
-				event.Skip();
-			}
-		}
-		else
-		{
-			if (!is_modified || (wxMessageBox("Really quit without saving changes?", "Quit", wxOK | wxCANCEL) == wxOK))
-			{
-				event.Skip();
-			}
-		}
-	}, wxID_EXIT);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		wxFileName help_file = wxFileName(wxStandardPaths::Get().GetResourcesDir(), "seqer.html");
-
-		if (!help_file.FileExists())
-		{
-			help_file = wxFileName(wxStandardPaths::Get().GetExecutablePath());
-			help_file.SetFullName("seqer.html");
-		}
-
-		if (help_file.FileExists()) wxLaunchDefaultBrowser(help_file.GetFullPath());
-	}, wxID_HELP_CONTENTS);
-
-	this->Bind(wxEVT_COMMAND_MENU_SELECTED, [=](wxCommandEvent& WXUNUSED(event)) {
-		wxMessageBox("Seqer\na MIDI sequencer by Div Slomin\nprovided under terms of the BSD license", "About", wxOK);
-	}, wxID_ABOUT);
-
-	this->Bind(wxEVT_CHAR_HOOK, [=](wxKeyEvent& event) {
-		int keycode = event.GetKeyCode();
-		int modifiers = event.GetModifiers();
-
-		if ((keycode == WXK_UP) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_DOWN) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_PAGEUP) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_PAGEDOWN) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_HOME) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == WXK_END) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == WXK_LEFT) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_RIGHT) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_HOME) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_END) && (modifiers == wxMOD_NONE))
-		{
-		}
-		else if ((keycode == WXK_UP) && (modifiers == wxMOD_ALT))
-		{
-		}
-		else if ((keycode == WXK_DOWN) && (modifiers == wxMOD_ALT))
-		{
-		}
-#ifdef __WXOSX__
-		else if ((keycode == WXK_UP) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == WXK_DOWN) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == WXK_LEFT) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == WXK_RIGHT) && (modifiers == wxMOD_CONTROL))
-		{
-		}
-		else if ((keycode == 'A') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'E') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'P') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'N') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'B') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'F') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'D') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-		else if ((keycode == 'H') && (modifiers == wxMOD_RAW_CONTROL))
-		{
-		}
-#endif
-		else if ((keycode == '=') && ((modifiers & ~wxMOD_SHIFT) == wxMOD_CONTROL))
-		{
-			// make ctrl+plus ignore whether shift is pressed
-			this->ProcessCommand(wxID_ZOOM_IN);
-		}
-		else if ((keycode == '-') && ((modifiers & ~wxMOD_SHIFT) == wxMOD_CONTROL))
-		{
-			// make ctrl+minus ignore whether shift is pressed
-			this->ProcessCommand(wxID_ZOOM_OUT);
-		}
-		else if ((keycode == WXK_BACK) && (modifiers == wxMOD_NONE))
-		{
-			// make backspace be a synonym for delete
-			this->ProcessCommand(wxID_DELETE);
-		}
-		else if ((keycode == ']') && (modifiers == wxMOD_NONE))
-		{
-			// although these are listed in the menu, they don't work automatically on WXGTK
-			this->ProcessCommand(SEQER_ID_SMALL_INCREASE);
-		}
-		else if ((keycode == '[') && (modifiers == wxMOD_NONE))
-		{
-			this->ProcessCommand(SEQER_ID_SMALL_DECREASE);
-		}
-		else if ((keycode == ']') && (modifiers == wxMOD_SHIFT))
-		{
-			this->ProcessCommand(SEQER_ID_LARGE_INCREASE);
-		}
-		else if ((keycode == '[') && (modifiers == wxMOD_SHIFT))
-		{
-			this->ProcessCommand(SEQER_ID_LARGE_DECREASE);
-		}
-		else
-		{
-			event.Skip();
-		}
-	});
-
-	this->Bind(wxEVT_CLOSE_WINDOW, [=](wxCloseEvent& event) {
-		if (!this->SaveChangesIfNeeded()) return;
-		event.Skip();
-	});
+	// This prevents the default behavior of showing unhelpful help text in the status bar when the user navigates the menu.
+	this->Bind(wxEVT_MENU_HIGHLIGHT, [=](wxMenuEvent&) {});
 
 	this->sequence_editor = new SequenceEditor(this, (existing_window == NULL) ? NULL : existing_window->sequence_editor);
-}
-
-bool Window::SaveChangesIfNeeded()
-{
-	if (!(this->sequence_editor->IsModified() && this->sequence_editor->IsLastEditorForSequence())) return true;
-	return this->SaveChanges();
-}
-
-bool Window::SaveChanges()
-{
-	switch (wxMessageBox("Do you want to save changes to the current file?", "Save Changes", wxYES_NO | wxCANCEL))
-	{
-		case wxYES:
-		{
-			return this->Save();
-		}
-		case wxCANCEL:
-		{
-			return false;
-		}
-		default:
-		{
-			return true;
-		}
-	}
-}
-
-bool Window::Save()
-{
-	if (this->sequence_editor->GetFilename() == wxEmptyString)
-	{
-		return this->SaveAs();
-	}
-	else
-	{
-		if (this->sequence_editor->Save())
-		{
-			return true;
-		}
-		else
-		{
-			wxMessageBox("Cannot save the specified MIDI file.", "Error", wxOK | wxICON_ERROR);
-			return false;
-		}
-	}
-}
-
-bool Window::SaveAs()
-{
-	wxFileDialog* file_dialog = new wxFileDialog(this, "Save File", "", "", "MIDI Files (*.mid)|*.mid|All files (*.*)|*.*", wxFD_SAVE);
-	bool result;
-
-	if (file_dialog->ShowModal() == wxID_OK)
-	{
-		if (this->sequence_editor->SaveAs(file_dialog->GetPath()))
-		{
-			result = true;
-		}
-		else
-		{
-			wxMessageBox("Cannot save the specified MIDI file.", "Error", wxOK | wxICON_ERROR);
-			result = false;
-		}
-	}
-	else
-	{
-		return false;
-	}
-
-	delete file_dialog;
-	return result;
 }
 
 Window::~Window()
