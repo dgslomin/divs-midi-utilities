e98fec3cec0c21617576047367d2b96594741343
Div Slomin <daslomin@microsoft.com>
2021-01-11 18:41:28 -0500

    more

--- src/qwertymidi/rtmidi/qwertymidi.cpp
+++ src/qwertymidi/rtmidi/qwertymidi.cpp
@@ -8,6 +8,14 @@
 #include <midiutil-rtmidi.h>
 
 static wxTextCtrl* text_box;
+static int startup_error = 0;
+static RtMidiOutPtr midi_out = NULL;
+static int channel_number = 0;
+static int program_number = -1;
+static int velocity = 64;
+static int map[128];
+static int down[128];
+static int transposition = 0;
 
 static void handle_key_down(wxKeyEvent& event)
 {
@@ -18,9 +26,10 @@ static void handle_key_up(wxKeyEvent& event)
 {
 }
 
-static void usage(char *program_name)
+static void usage(wxString program_name)
 {
-	text_box->SetValue(wxString::Format("Usage:  %s --out <port> [ --channel <n> ] [ --program <n> ] [ --velocity <n> ] [ --map <filename> ]\n", program_name));
+	text_box->SetValue(wxString::Format("Usage:  %s [ --out <port> ] [ --channel <n> ] [ --program <n> ] [ --velocity <n> ] [ --map <filename> ]\n", program_name));
+	startup_error = 1;
 }
 
 class Application: public wxApp
@@ -32,34 +41,39 @@ public:
 bool Application::OnInit()
 {
 	wxFrame *window = new wxFrame(NULL, wxID_ANY, "Qwertymidi", wxDefaultPosition, wxSize(640, 480));
-	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-	text_box = new wxTextCtrl(window, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
-	sizer->Add(text_box, wxSizerFlags().Proportion(1).Expand().Border());
-	window->SetSizer(sizer);
-	text_box->Bind(wxEVT_KEY_DOWN, handle_key_down);
-	text_box->Bind(wxEVT_KEY_UP, handle_key_up);
-	this->SetTopWindow(window);
-	window->Show(true);
+	text_box = new wxTextCtrl(window, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY | wxBORDER_NONE);
 
 	for (int i = 1; i < this->argc; i++)
 	{
-		else if (strcmp(this->argv[i], "--out") == 0)
+		if (strcmp(this->argv[i].char_str(), "--out") == 0)
 		{
-			if (++i == this->argc)
+			if (++i == this->argc) usage(this->argv[0]);
+
+			if ((midi_out = rtmidi_open_out_port((char *)("qwertymidi"), this->argv[i].char_str(), (char *)("qwertymidi"))) == NULL)
 			{
-				usage(this->argv[0]);
-			}
-			else
-			{
-				if ((midi_out = rtmidi_open_out_port("notemap", this->argv[i], "notemap")) == NULL)
-				{
-					fprintf(stderr, "Error:  Cannot open MIDI output port \"%s\".\n", this->argv[i]);
-					exit(1);
-				}
+				text_box->SetValue(wxString::Format("Error:  Cannot open MIDI output port \"%s\".\n", this->argv[i]));
+				startup_error = 1;
 			}
 		}
-		else if (strcmp(this->argv[i], "--map") == 0)
+		else if (strcmp(this->argv[i].char_str(), "--channel") == 0)
 		{
+			if (++i == this->argc) usage(this->argv[0]);
+			channel_number = atoi(this->argv[i].char_str());
+		}
+		else if (strcmp(this->argv[i].char_str(), "--program") == 0)
+		{
+			if (++i == this->argc) usage(this->argv[0]);
+			program_number = atoi(this->argv[i].char_str());
+		}
+		else if (strcmp(this->argv[i].char_str(), "--velocity") == 0)
+		{
+			if (++i == this->argc) usage(this->argv[0]);
+			velocity = atoi(argv[i].char_str());
+		}
+		else if (strcmp(this->argv[i].char_str(), "--map") == 0)
+		{
+			if (++i == this->argc) usage(this->argv[0]);
+			// TODO
 		}
 		else
 		{
@@ -67,6 +81,14 @@ bool Application::OnInit()
 		}
 	}
 
+	if (!startup_error)
+	{
+		text_box->Bind(wxEVT_KEY_DOWN, handle_key_down);
+		text_box->Bind(wxEVT_KEY_UP, handle_key_up);
+	}
+
+	this->SetTopWindow(window);
+	window->Show(true);
 	return true;
 }
 
