22e0264dc4b2e58f143087e230f77954177bb2ab
Div Slomin <dgslomin@alumni.princeton.edu>
2021-01-14 02:03:57 -0500

    joycc: rtmidi (works, but has runaway CPU usage and doesn't recognize d-pad)

--- src/joycc/rtmidi/joycc.cpp
+++ src/joycc/rtmidi/joycc.cpp
@@ -3,128 +3,123 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wx/wx.h>
+#include <wx/event.h>
+#include <wx/joystick.h>
 #include <rtmidi_c.h>
-#include <expat.h>
 #include <midiutil-common.h>
 #include <midiutil-rtmidi.h>
-#include <midiutil-expat.h>
 #include <midiutil-wx.h>
 
-#define ACTION_REST 10000
-#define ACTION_EXIT 10001
-
 static wxTextCtrl* text_box;
+static wxTimer* timer;
 static int startup_error = 0;
 static RtMidiOutPtr midi_out = NULL;
 static int channel_number = 0;
-static int program_number = -1;
-static int velocity = 64;
-static int map[512];
-static int current_note = 63;
+static int number_of_joysticks = 0;
+static wxJoystick* joystick_array[2];
+static int axis_controller_number_array[2][6];
+static int button_controller_number_array[2][64];
+static int axis_last_value_array[2][6];
+static int button_last_value_array[2][64];
 
-static void handle_xml_start_element(void *user_data, const XML_Char *name, const XML_Char **attributes)
-{
-	if (strcmp(name, "map") == 0)
-	{
-		int key = -1;
-		wxString action = wxEmptyString;
-
-		for (int i = 0; attributes[i] != NULL; i += 2)
-		{
-			if (strcmp(attributes[i], "key") == 0)
-			{
-				key = MidiUtil_getWxKeyCodeFromName(attributes[i + 1]);
-			}
-			else if (strcmp(attributes[i], "action") == 0)
-			{
-				action = attributes[i + 1];
-			}
-		}
-
-		if ((key >= 0) && (action != wxEmptyString))
-		{
-			if (action == "rest")
-			{
-				map[key] = ACTION_REST;
-			}
-			else if (action == "exit")
-			{
-				map[key] = ACTION_EXIT;
-			}
-			else
-			{
-				map[key] = wxAtoi(action);
-			}
-		}
-	}
-}
-
-static void send_note_on(int channel, int note, int velocity)
+static void send_control_change(int channel, int number, int value)
 {
 	if (midi_out != NULL)
 	{
-		unsigned char message[MIDI_UTIL_MESSAGE_SIZE_NOTE_ON];
-		MidiUtilMessage_setNoteOn(message, channel, note, velocity);
-		rtmidi_out_send_message(midi_out, message, MIDI_UTIL_MESSAGE_SIZE_NOTE_ON);
+		unsigned char message[MIDI_UTIL_MESSAGE_SIZE_CONTROL_CHANGE];
+		MidiUtilMessage_setControlChange(message, channel, number, value);
+		rtmidi_out_send_message(midi_out, message, MIDI_UTIL_MESSAGE_SIZE_CONTROL_CHANGE);
 	}
 }
 
-static void send_note_off(int channel, int note)
+static int get_joystick_axis_min(wxJoystick* joystick, int axis_number)
 {
-	if (midi_out != NULL)
-	{
-		unsigned char message[MIDI_UTIL_MESSAGE_SIZE_NOTE_OFF];
-		MidiUtilMessage_setNoteOff(message, channel, note, 0);
-		rtmidi_out_send_message(midi_out, message, MIDI_UTIL_MESSAGE_SIZE_NOTE_OFF);
+	switch (axis_number) {
+		case 0: return joystick->GetXMin();
+		case 1: return joystick->GetYMin();
+		case 2: return joystick->GetZMin();
+		case 3: return joystick->GetRudderMin();
+		case 4: return joystick->GetUMin();
+		case 5: return joystick->GetVMin();
+		default: return 0;
 	}
 }
 
-static void send_program_change(int channel, int number)
+static int get_joystick_axis_max(wxJoystick* joystick, int axis_number)
 {
-	if (midi_out != NULL)
-	{
-		unsigned char message[MIDI_UTIL_MESSAGE_SIZE_PROGRAM_CHANGE];
-		MidiUtilMessage_setProgramChange(message, channel, number);
-		rtmidi_out_send_message(midi_out, message, MIDI_UTIL_MESSAGE_SIZE_PROGRAM_CHANGE);
+	switch (axis_number) {
+		case 0: return joystick->GetXMax();
+		case 1: return joystick->GetYMax();
+		case 2: return joystick->GetZMax();
+		case 3: return joystick->GetRudderMax();
+		case 4: return joystick->GetUMax();
+		case 5: return joystick->GetVMax();
+		default: return 0;
 	}
 }
 
-static void handle_key_down(wxKeyEvent& event)
+static void handle_timer(wxTimerEvent& event)
 {
-	int key_code = event.GetKeyCode();
-	wxString key_name = MidiUtil_getNameFromWxKeyCode((wxKeyCode)(key_code));
-	int action = map[key_code];
+	wxString status_message = wxEmptyString;
+	int joystick_number;
 
-	switch (action)
+	for (joystick_number = 0; joystick_number < number_of_joysticks; joystick_number++)
 	{
-		case ACTION_REST:
+		wxJoystick* joystick = joystick_array[joystick_number];
+		status_message.append(wxString::Format("joystick %d:\n\naxes:", joystick_number));
+
 		{
-			send_note_off(channel_number, current_note);
-			text_box->SetValue(wxString::Format("<map key=\"%s\" action=\"rest\" />", key_name));
-			break;
+			int number_of_axes = joystick->GetMaxAxes();
+			int axis_number;
+
+			for (axis_number = 0; axis_number < number_of_axes; axis_number++)
+			{
+				int position = joystick->GetPosition(axis_number);
+				int min = get_joystick_axis_min(joystick, axis_number);
+				int max = get_joystick_axis_max(joystick, axis_number);
+				int value = (position - min) * 128 / (max - min);
+
+				if (value != axis_last_value_array[joystick_number][axis_number])
+				{
+					int controller_number = axis_controller_number_array[joystick_number][axis_number];
+					if (controller_number >= 0) send_control_change(channel_number, controller_number, value);
+					axis_last_value_array[joystick_number][axis_number] = value;
+				}
+
+				status_message.append(wxString::Format("%s %d=%d", (axis_number == 0) ? "" : ",", axis_number, value));
+			}
 		}
-		case ACTION_EXIT:
+
+		status_message.append("\n\nbuttons:");
+
 		{
-			send_note_off(channel_number, current_note);
-			wxExit();
-			break;
+			int number_of_buttons = joystick->GetMaxButtons();
+			int button_number;
+
+			for (button_number = 0; button_number < number_of_buttons; button_number++)
+			{
+				int value = joystick->GetButtonState(button_number) ? 127 : 0;
+
+				if (value != button_last_value_array[joystick_number][button_number])
+				{
+					int controller_number = button_controller_number_array[joystick_number][button_number];
+					if (controller_number >= 0) send_control_change(channel_number, controller_number, value);
+					button_last_value_array[joystick_number][button_number] = value;
+				}
+
+				status_message.append(wxString::Format("%s %d=%d", (button_number == 0) ? "" : ",", button_number, value));
+			}
 		}
-		default:
-		{
-			char note_name[128];
-			send_note_off(channel_number, current_note);
-			current_note = MidiUtil_clamp(current_note + action, 0, 127);
-			send_note_on(channel_number, current_note, velocity);
-			MidiUtil_setNoteNameFromNumber(action, note_name);
-			text_box->SetValue(wxString::Format("<map key=\"%s\" action=\"%d\" />", key_name, action));
-			break;
-		}
+
+		status_message.append("\n\n");
 	}
+
+	text_box->SetValue(status_message);
 }
 
 static void usage(wxString program_name)
 {
-	text_box->SetValue(wxString::Format("Usage: %s [ --out <n> ] [ --channel <n> ] [ --x1 <n> ] [ --y1 <n> ] [ --z1 <n> ] [ --a1 <n> ] [ --b1 <n> ] [ --c1 <n> ] [ --d1 <n> ] [ --x2 <n> ] [ --y2 <n> ] [ --z2 <n> ] [ --a2 <n> ] [ --b2 <n> ] [ --c2 <n> ] [ --d2 <n> ]\n", program_name));
+	text_box->SetValue(wxString::Format("Usage: %s [ --out <n> ] [ --channel <n> ] [ --axis <joystick number> <axis number> <controller number> ] ... [ --button <joystick number> <button number> <controller number> ] ...\n", program_name));
 	startup_error = 1;
 }
 
@@ -138,10 +133,10 @@ public:
 bool Application::OnInit()
 {
 	int i;
-	wxFrame *window = new wxFrame(NULL, wxID_ANY, "Delta", wxDefaultPosition, wxSize(640, 480));
+	wxFrame *window = new wxFrame(NULL, wxID_ANY, "Joycc", wxDefaultPosition, wxSize(640, 480));
 	text_box = new wxTextCtrl(window, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY | wxBORDER_NONE);
-
-	for (i = 0; i < 512; i++) map[i] = ACTION_REST;
+	timer = new wxTimer(text_box);
+	text_box->Bind(wxEVT_TIMER, handle_timer);
 
 	for (i = 1; i < this->argc; i++)
 	{
@@ -149,7 +144,7 @@ bool Application::OnInit()
 		{
 			if (++i == this->argc) usage(this->argv[0]);
 
-			if ((midi_out = rtmidi_open_out_port((char *)("delta"), this->argv[i].char_str(), (char *)("delta"))) == NULL)
+			if ((midi_out = rtmidi_open_out_port((char *)("joycc"), this->argv[i].char_str(), (char *)("joycc"))) == NULL)
 			{
 				text_box->SetValue(wxString::Format("Error:  Cannot open MIDI output port \"%s\".\n", this->argv[i]));
 				startup_error = 1;
@@ -160,29 +155,27 @@ bool Application::OnInit()
 			if (++i == this->argc) usage(this->argv[0]);
 			channel_number = wxAtoi(this->argv[i]);
 		}
-		else if (this->argv[i] == "--program")
+		else if (this->argv[i] == "--axis")
 		{
+			int joystick_number, axis_number, controller_number;
 			if (++i == this->argc) usage(this->argv[0]);
-			program_number = wxAtoi(this->argv[i]);
+			joystick_number = wxAtoi(this->argv[i]);
+			if (++i == this->argc) usage(this->argv[0]);
+			axis_number = wxAtoi(this->argv[i]);
+			if (++i == this->argc) usage(this->argv[0]);
+			controller_number = wxAtoi(this->argv[i]);
+			axis_controller_number_array[joystick_number][axis_number] = controller_number;
 		}
-		else if (this->argv[i] == "--velocity")
+		else if (this->argv[i] == "--button")
 		{
+			int joystick_number, button_number, controller_number;
 			if (++i == this->argc) usage(this->argv[0]);
-			velocity = wxAtoi(this->argv[i]);
-		}
-		else if (this->argv[i] == "--map")
-		{
-			XML_Parser xml_parser = XML_ParserCreate(NULL);
+			joystick_number = wxAtoi(this->argv[i]);
 			if (++i == this->argc) usage(this->argv[0]);
-			XML_SetStartElementHandler(xml_parser, handle_xml_start_element);
-
-			if (XML_ParseFile(xml_parser, argv[i].char_str()) < 0)
-			{
-				text_box->SetValue(wxString::Format("Error:  \"%s\" at line %d of map file \"%s\"\n", XML_ErrorString(XML_GetErrorCode(xml_parser)), XML_GetCurrentLineNumber(xml_parser), argv[i]));
-				startup_error = 1;
-			}
-
-			XML_ParserFree(xml_parser);
+			button_number = wxAtoi(this->argv[i]);
+			if (++i == this->argc) usage(this->argv[0]);
+			controller_number = wxAtoi(this->argv[i]);
+			button_controller_number_array[joystick_number][button_number] = controller_number;
 		}
 		else
 		{
@@ -192,8 +185,18 @@ bool Application::OnInit()
 
 	if (!startup_error)
 	{
-		if (program_number >= 0) send_program_change(channel_number, program_number);
-		text_box->Bind(wxEVT_KEY_DOWN, handle_key_down);
+		int joystick_number, axis_number, button_number;
+		number_of_joysticks = wxJoystick::GetNumberJoysticks();
+
+		for (joystick_number = 0; joystick_number < number_of_joysticks; joystick_number++)
+		{
+			wxJoystick* joystick = new wxJoystick(joystick_number);
+			joystick_array[joystick_number] = joystick;
+			for (axis_number = 0; axis_number < 6; axis_number++) axis_last_value_array[joystick_number][axis_number] = 0;
+			for (button_number = 0; button_number < 64; button_number++) button_last_value_array[joystick_number][button_number] = 0;
+		}
+
+		timer->Start(100);
 	}
 
 	this->SetTopWindow(window);
@@ -204,6 +207,14 @@ bool Application::OnInit()
 int Application::OnExit()
 {
 	if (midi_out != NULL) rtmidi_close_port(midi_out);
+	delete timer;
+
+	while (number_of_joysticks > 0)
+	{
+		wxJoystick* joystick = joystick_array[--number_of_joysticks];
+		delete joystick;
+	}
+
 	return 0;
 }
 
