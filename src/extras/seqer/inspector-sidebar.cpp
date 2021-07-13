63bdbbdaa3ef2853be0ca5e76026621f5deb50c0
Div Slomin <daslomin@microsoft.com>
2021-07-13 19:19:56 -0400

    seqer: tweak

--- src/extras/seqer/inspector-sidebar.cpp
+++ src/extras/seqer/inspector-sidebar.cpp
@@ -12,14 +12,16 @@
 InspectorSidebar::InspectorSidebar(Window* window)
 {
 	this->window = window;
-	QVBoxLayout* outer_layout = new QVBoxLayout(this);
-	QMargins outer_layout_contents_margins = outer_layout->contentsMargins();
-	outer_layout_contents_margins.setLeft(0);
-	outer_layout_contents_margins.setRight(0);
-	outer_layout->setContentsMargins(outer_layout_contents_margins);
 
-	QFormLayout* lane_layout = new QFormLayout();
-	outer_layout->addLayout(lane_layout);
+	QVBoxLayout* outer_layout = new QVBoxLayout(this);
+	outer_layout->setSpacing(0);
+	outer_layout->setContentsMargins(0, 0, 0, 0);
+
+	QWidget* lane_panel = new QWidget();
+	outer_layout->addWidget(lane_panel);
+	QFormLayout* lane_layout = new QFormLayout(lane_panel);
+	lane_layout->setSpacing(4);
+	lane_layout->setContentsMargins(8, 8, 8, 8);
 
 	QLineEdit* lane_type_line_edit = new QLineEdit();
 	lane_layout->addRow(tr("Lane Type"), lane_type_line_edit);
@@ -33,8 +35,11 @@ InspectorSidebar::InspectorSidebar(Window* window)
 	separator->setLineWidth(0);
 	separator->setForegroundRole(QPalette::Mid);
 
-	QFormLayout* event_layout = new QFormLayout();
-	outer_layout->addLayout(event_layout, 1);
+	QWidget* event_panel = new QWidget();
+	outer_layout->addWidget(event_panel, 1);
+	QFormLayout* event_layout = new QFormLayout(event_panel);
+	event_layout->setSpacing(4);
+	event_layout->setContentsMargins(8, 8, 8, 8);
 
 	QLineEdit* event_type_line_edit = new QLineEdit();
 	event_layout->addRow(tr("Event Type"), event_type_line_edit);
